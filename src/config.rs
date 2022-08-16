895fc7da95ff813214cf725437ea0d6be0a5d70b
Alex Snaps <alex@wcgw.dev>
2022-08-15 21:36:28 -0400

    Proper fmt

--- src/config.rs
+++ src/config.rs
@@ -21,23 +21,23 @@ use std::collections::HashMap;
 
 #[derive(Deserialize)]
 pub struct Config {
-    pub teams: HashMap<String, Vec<Member>>,
+  pub teams: HashMap<String, Vec<Member>>,
 }
 
 #[derive(Deserialize)]
 pub struct Member {
-    pub name: String,
-    pub location: chrono_tz::Tz,
+  pub name: String,
+  pub location: chrono_tz::Tz,
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::config::Config;
+  use crate::config::Config;
 
-    #[test]
-    fn reads_config_alright() {
-        let config: Config = toml::from_str(
-            r#"
+  #[test]
+  fn reads_config_alright() {
+    let config: Config = toml::from_str(
+      r#"
     [[teams.wcgw]]
     name = "Alex"
     location = "America/Montreal"
@@ -50,41 +50,32 @@ mod tests {
     name = "John Doe"
     location = "Europe/Dublin"
     "#,
-        )
-        .unwrap();
+    )
+    .unwrap();
 
-        assert_eq!(config.teams.len(), 2);
-        assert!(config.teams.contains_key("wcgw"));
-        assert!(config.teams.contains_key("managers"));
+    assert_eq!(config.teams.len(), 2);
+    assert!(config.teams.contains_key("wcgw"));
+    assert!(config.teams.contains_key("managers"));
 
-        assert_eq!(config.teams.get("wcgw").unwrap().len(), 2);
-        assert_eq!(config.teams.get("managers").unwrap().len(), 1);
+    assert_eq!(config.teams.get("wcgw").unwrap().len(), 2);
+    assert_eq!(config.teams.get("managers").unwrap().len(), 1);
 
-        assert_eq!(
-            config.teams.get("managers").unwrap().first().unwrap().name,
-            "John Doe"
-        );
-        assert_eq!(
-            config
-                .teams
-                .get("managers")
-                .unwrap()
-                .first()
-                .unwrap()
-                .location,
-            chrono_tz::Tz::Europe__Dublin
-        );
+    assert_eq!(config.teams.get("managers").unwrap().first().unwrap().name, "John Doe");
+    assert_eq!(
+      config.teams.get("managers").unwrap().first().unwrap().location,
+      chrono_tz::Tz::Europe__Dublin
+    );
 
-        assert_eq!(config.teams.get("wcgw").unwrap()[0].name, "Alex");
-        assert_eq!(
-            config.teams.get("wcgw").unwrap()[0].location,
-            chrono_tz::Tz::America__Montreal
-        );
+    assert_eq!(config.teams.get("wcgw").unwrap()[0].name, "Alex");
+    assert_eq!(
+      config.teams.get("wcgw").unwrap()[0].location,
+      chrono_tz::Tz::America__Montreal
+    );
 
-        assert_eq!(config.teams.get("wcgw").unwrap()[1].name, "John Doe");
-        assert_eq!(
-            config.teams.get("wcgw").unwrap()[1].location,
-            chrono_tz::Tz::Europe__Dublin
-        );
-    }
+    assert_eq!(config.teams.get("wcgw").unwrap()[1].name, "John Doe");
+    assert_eq!(
+      config.teams.get("wcgw").unwrap()[1].location,
+      chrono_tz::Tz::Europe__Dublin
+    );
+  }
 }
